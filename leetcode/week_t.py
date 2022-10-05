09ed3203c56c7738db59eb6b4401dfc47474d873
Alan_Huawei_Cup <XiangDongxu1024@163.com>
2022-10-05 12:17:38 +0800

    update template.

--- leetcode/week_t.py
+++ leetcode/week_t.py
@@ -6,11 +6,11 @@ from bisect import bisect_left, bisect_right
 from functools import reduce
 from heapq import heappush, heappop
 from math import comb
-
 from numpy import lcm
+
 from sortedcontainers import SortedList
 
-lowbit = lambda x: x & (-x)
+lowbit = lambda x: x & -x
 
 
 class Solution:
