4d1134f336b3e9c144150c91499a6f343060f234
Alan_Huawei_Cup <XiangDongxu1024@163.com>
2022-10-05 11:57:56 +0800

    add weekly contest template.

---
+++ leetcode/week_t.py
@@ -0,0 +1,29 @@
+# encoding: utf-8
+# author: Alan-learner
+
+import math
+from bisect import bisect_left, bisect_right
+from functools import reduce
+from heapq import heappush, heappop
+from math import comb
+
+from numpy import lcm
+from sortedcontainers import SortedList
+
+lowbit = lambda x: x & (-x)
+
+# Binary Index Tree
+
+
+class Solution:
+    pass
+
+
+def main():
+    s = Solution()
+    res = s
+    print(res)
+
+
+if __name__ == '__main__':
+    main()
