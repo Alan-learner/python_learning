16ff722db498c6ebfcac4481e9e4764a173296cd
Alan_Huawei_Cup <XiangDongxu1024@163.com>
2022-10-16 10:12:30 +0800

    update weekly contest template.

--- leetcode/week_t.py
+++ leetcode/week_t.py
@@ -8,11 +8,15 @@ from bisect import bisect_left, bisect_right
 from functools import reduce
 from heapq import heappush, heappop
 from math import comb
+from executing import cache
+from yarl import cache_clear
+
 from numpy import lcm
 
 from sortedcontainers import SortedList
 
 lowbit = lambda x: x & -x
+MOD = 10 ** 9 + 7
 
 
 class Solution:
