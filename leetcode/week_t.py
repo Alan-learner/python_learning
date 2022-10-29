f895d93ca91b874859ba203728466ffc7148449d
Alan_Huawei_Cup <XiangDongxu1024@163.com>
2022-10-29 11:14:45 +0800

    update leetcode contest template.

--- leetcode/week_t.py
+++ leetcode/week_t.py
@@ -7,7 +7,7 @@ from math import inf
 from typing import List
 from bisect import bisect_left, bisect_right
 from functools import reduce
-from heapq import heappush, heappop
+from heapq import heappush, heappop, heapreplace, heapify
 from math import comb
 from executing import cache
 from yarl import cache_clear
@@ -15,10 +15,12 @@ from yarl import cache_clear
 from numpy import lcm
 
 from sortedcontainers import SortedList
+from itertools import permutations, combinations
 
 lowbit = lambda x: x & -x
 MOD = int(1e9 + 7)
 INF = int(1e20)
+
 
 class Solution:
     pass
