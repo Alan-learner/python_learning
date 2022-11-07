9367ebeb1000229c6c6d1c54e9a64e89c552bb57
Alan_Huawei_Cup <XiangDongxu1024@163.com>
2022-11-07 22:27:50 +0800

    update leeccode weekly contest template.

--- leetcode/week_t.py
+++ leetcode/week_t.py
@@ -2,9 +2,9 @@
 # author: Alan-learner
 
 import math
-from collections import deque
+from collections import deque, defaultdict
 from math import inf
-from typing import List
+from typing import List, Optional, Counter
 from bisect import bisect_left, bisect_right
 from functools import reduce
 from heapq import heappush, heappop, heapreplace, heapify
